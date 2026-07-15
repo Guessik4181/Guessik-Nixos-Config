478c10d729917252b5f6c2e27640db481da26ab0
Guessik <GuessWhat4181@gmail.com>
2026-07-15 14:19:14 +0200

    fucked something up

--- homeModules/KDEconnect.nix
+++ homeModules/KDEconnect.nix
@@ -1,6 +1,6 @@
 { pkgs, systemSettings, ... }:
 {
-home-manager.users.guessik.services.kdeconnect.enable = true;
+services.kdeconnect.enable = true;
 
 networking.firewall = rec {
   allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
